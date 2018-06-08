7eac8eee91a7f6b3874056be75b1b7ee8d3cc76f
Nastassja Kuznetsova <nastassjakuz@gmail.com>
2018-06-08 08:05:59 -0400

    cube + vr setup

---
+++ SpyShortage/Assets/Scripts/Appear.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appear : MonoBehaviour
+{
+
+    public GameObject sampleObject;
+
+    public void AddObject()
+    {
+     //   Instantiate(sampleObject, Vector3.zero, Quaternion.Identity);
+    }
+}
